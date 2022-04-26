74ec2d675d18b24d171ee1b44f6fc0532b7b5f2f
lekimb <92205383+lekimb@users.noreply.github.com>
2022-04-26 14:21:13 +0200

    Update and rename Reverse String.js to Reverse_String.js

--- Strings/Reverse_String.js
+++ Strings/Reverse_String.js
@@ -7,7 +7,7 @@ var reverseString = function(s) {
     let right = s.length - 1;
     
     while ( left < right){
-        let temp = s[left]; // temporary value
+        let temp = s[left];
         s[left] = s[right];
         s[right] = temp;
         left++;
